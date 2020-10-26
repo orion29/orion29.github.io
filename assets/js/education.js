1a4643e656a589fd09c92755c5665f57dbaeea17
Kunal <47053809+orion29@users.noreply.github.com>
2020-10-27 04:51:07 +0530

    Add files via upload

---
+++ assets/js/education.js
@@ -0,0 +1,172 @@
+AOS.init();
+
+// MOOCs Cards
+
+const moocs = document.querySelector(".moocs");
+const moocscards = [
+  {
+    title: "Machine Learning",
+    cardImage: "assets/images/education-page/coursera1.png",
+    moocLink: "https://www.coursera.org/learn/machine-learning",
+  },
+  
+  
+  {
+    title: "Machine Learning with Python",
+    cardImage: "assets/images/education-page/cog.webp",
+    moocLink: "https://cognitiveclass.ai/courses/machine-learning-with-python",
+  },
+  {
+    title: "Deep Learning Fundamentals",
+    cardImage: "assets/images/education-page/cog.webp",
+    moocLink: "https://cognitiveclass.ai/courses/introduction-deep-learning",
+  },
+  {
+    title: "Introduction to Deep Learning with PyTorch",
+    cardImage: "assets/images/education-page/udacity_logo.png",
+    moocLink: "https://www.udacity.com/course/deep-learning-pytorch--ud188",
+  },
+  {
+    title: "Convolutional Neural Network",
+    cardImage: "assets/images/education-page/coursera1.png",
+    moocLink: "https://www.coursera.org/learn/convolutional-neural-networks",
+  },
+
+  {
+    title: "Sequence Models",
+    cardImage: "assets/images/education-page/coursera1.png",
+    moocLink: "https://www.coursera.org/learn/nlp-sequence-models",
+  },
+  
+  
+  
+  {
+    title: "fastai course-v4 (2020)",
+    cardImage: "assets/images/education-page/fast.jpg",
+    moocLink: "https://course.fast.ai/videos",
+  },
+  {
+    title: "CS230 Deep Learning",
+    cardImage: "assets/images/education-page/stanford.jpg",
+    moocLink: "https://www.youtube.com/watch?v=PySo_6S4ZAg&list=PLoROMvodv4rOABXSygHTsbvUz4G_YQhOb",
+  },
+  
+  
+  
+ 
+];
+
+const showCards = () => {
+  let output = "";
+  moocscards.forEach(
+    ({ title, cardImage,moocLink }) =>
+      (output += `        
+        <div class="col-6 col-md-3 col-sm-4 column" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600" >  
+            <div class="card mb-3 mx-auto">
+               <div class="content">
+                  <div class="content-overlay"></div>
+                    <img src=${cardImage} class="card-img-top content-image">     
+                  <div class="content-details fadeIn-bottom">
+                    <a href="${moocLink}" target="_blank"><i class="fa fa-info-circle fa-2x" aria-hidden="true" style="color: white;"></i></a>                                   
+                  </div>
+                </div>
+                <div class="card-body">
+                    <h6 class="mt-0 py-2 text-center font-weight-bold mooc-title" style="font-size:12px;">${title}</h6>
+                </div>
+            </div>
+        </div>        
+      `)
+  );
+  moocs.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards);
+
+
+/* Badges*/
+
+const bagdes = document.querySelector(".badges");
+const badgesection = [
+  {
+    title: "Machine Learning with Python ",
+    image: "https://images.youracclaim.com/images/53caf8cc-b5e9-4424-b4a7-7b069fa13db4/Machine_Learning_with_Python.png",
+    description: "Earned May 20, 2020",
+  },
+ 
+];
+
+const showCards1 = () => {
+  let output = "";
+  badgesection.forEach(
+    ({ title, image, description}) =>
+      (output += `       
+      <div class="col-lg-4 col-md-6 p-2" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600"> 
+        <img class="img-fluid d-block mb-3 mx-auto hvr-grow" src="${image}" alt="Card image cap" width="200">
+          <div class="text-center font-weight-bolder" style="font-size: 1.3em;">${title}</div>
+          <div class="text-center text-muted font-weight-bolder p-2">${description}</div>
+      </div>`)
+  );
+  bagdes.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards1);
+
+
+/* Timeline Section*/
+
+const timeline = document.querySelector(".timeline");
+const timelinesection = [
+
+  {
+    heading: "Guru Gobind Singh Indraprastha University",
+    image: "./assets/images/education-page/timeline-1.svg",
+    subheading:"Bachelor of Technology (Electronics & Communication), (2017-2021)",
+    description: "CGPA: 8.3/10",
+  },
+  {
+    heading: "V.N.B.S.S School",
+    image: "./assets/images/education-page/timeline-1.svg",
+    subheading:"12th Standard, (2015-2017)",
+    description: "Percentage: 90%",
+  },
+  {
+    heading: "Greenfields School",
+    image: "./assets/images/education-page/timeline-1.svg",
+    subheading:"High School, (2013-2015)",
+    description: "CGPA: 10/10",
+  },
+
+  
+
+
+
+  
+];
+
+const showCards2 = () => {
+  let output = "";
+  timelinesection.forEach(
+    ({ heading, image, subheading, description}) =>
+      (output += `       
+      <div class="timeline" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600">
+      <div class="timeline-content">
+        <h6 class="center-align font-weight-bolder pt-1">${heading}</h6>
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-sm-4">
+            <img src="${image}" alt="" height="110"
+              class=" mt-2 w-100 mx-auto">
+          </div>
+          <div class="col-lg-8 col-md-8 col-sm-8 px-4">
+            <span style="opacity: 0.9; font-size: 15px;">
+              ${subheading}
+            </span>
+            <h6 class="text-muted mt-2" style="font-size: small;">Grades:</h6>
+            <ol style="margin-top: -8px; margin-left: -10px; font-size: 12px; list-style: disc;">
+              ${description}
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>`)
+  );
+  timeline.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards2);
---
+++ assets/js/main.js
@@ -0,0 +1,194 @@
+//common side navbar call
+
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+});
+
+//Get the top button
+var mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+//Footer and Navbar
+
+let header = $(`
+<nav class="black">
+<div class="nav-wrapper">
+    <!-- <a href="#!" class="brand-logo"><img src="assets/images/John-Doe1.png"></a> -->
+    <a href="#" data-target="mobile-demo" class="white-text sidenav-trigger"><i
+            class="material-icons">menu</i></a>
+    <ul class="right hide-on-med-and-down">
+        <li><a class="white-text" href="index.html">Home</a></li>
+        <li><a class="white-text" href="experience.html">Experience</a></li>
+        <li><a class="white-text" href="projects.html">Projects</a></li>
+        <!-- <li><a class="white-text" href="research.html">Research</a></li> -->
+        <li><a class="white-text" href="education.html">Education</a></li>
+        <!--  <li><a class="white-text" href="index.html">Blog</a></li> -->
+        <li><a class="white-text" href="assets/Kunal_Jethuri.pdf" target="_blank">My Resume</a></li>
+        <!--TOGGLE FOR LIGHT/DARK MODE-->
+        <li>
+        <label for="switch" class="theme-switch">
+        <input class="toggle-checkbox" type="checkbox" id="switch" name="theme" />
+          <div class='toggle-slot'>
+          <div class='sun-icon-wrapper'>
+          <div class="iconify sun-icon" data-icon="feather-sun" data-inline="false"> </div>
+          </div>
+          <div class='toggle-button'></div>
+          <div class='moon-icon-wrapper'>
+          <div class="iconify moon-icon" data-icon="feather-moon" data-inline="false"> </div>
+          </div>
+          </div>
+          </label>
+        </li>
+    </ul>
+</div>
+</nav>
+
+<!--Side Nav Bar -->
+<ul class="sidenav" id="mobile-demo">
+<li><a href="index.html">Home</a></li>
+<li><a href="experience.html">Experience</a></li>
+<li><a href="projects.html">Projects</a></li>
+<li><a href="research.html">Research</a></li>
+<li><a href="education.html">Education</a></li>
+<li><a href="index.html">Blog</a></li>
+<li><a href="assets/Kunal_Jethuri.pdf" target="_blank">My Resume</a></li>
+
+
+<!--TOGGLE FOR LIGHT/DARK MODE-->
+        <li>
+        <label for="switch" class="theme-switch">
+        <input class="toggle-checkbox" type="checkbox" id="switch" name="theme" />
+          <div class='toggle-slot side-toggle'>
+          <div class='sun-icon-wrapper'>
+          <div class="iconify sun-icon" data-icon="feather-sun" data-inline="false"> </div>
+          </div>
+          <div class='toggle-button'></div>
+          <div class='moon-icon-wrapper'>
+          <div class="iconify moon-icon" data-icon="feather-moon" data-inline="false"> </div>
+          </div>
+          </div>
+          </label>
+        </li>
+</ul>`);
+
+let footer = $(`
+<footer class="webintern-footer">
+<div class="webintern-footer-inner">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-lg-7 col-md-6 pt-3" >
+        
+      <h4 class="webintern_footer_title h4" >
+      <small style="font-size:1.6vw">  Connect </small>
+    </h4>
+    
+      <div class="col-sm-6 col-md-7">
+        <a class="webintern-footer-logo" href="#">
+         <!-- <center><img src="assets/images/John-Doe1.png"style="width: 220px;"/></center> -->
+        </a>
+        <div class="simple-text text-center dark padding-sm">
+          
+        <div class="empty-space xs-25 sm-25"><br></div>
+      </div>
+    </div>
+    <!-- Social media icons for footer -->
+
+<div class="social-icons-footer">
+  
+  <a class="social-icon-footer linkedin" href="https://www.linkedin.com/in/kunal-jethuri-900a85181/" target="_blank" rel="author">
+    <i class="fab fa-linkedin-in"></i>
+      </a>
+  <a class="social-icon-footer kaggle" href="https://www.kaggle.com/orion29" target="_blank" rel="author">
+    <i class="fab fa-kaggle"></i>
+      </a>
+  <a class="social-icon-footer github" href="https://www.github.com/orion29" target="_blank" rel="author">
+    <i class="fab fa-github"></i>
+      </a>
+      <a class="social-icon-footer twitter" href="" target="_blank" rel="author">
+    <i class="fab fa-twitter"></i>
+      </a>
+  <br>
+<p style="color:white;><a href=" "></a></p>
+</div>
+    </div>
+
+      <div class="col-sm-6 col-md-5 pt-3 footer-2">
+      <h4 class="webintern_footer_title h4" >
+      <small style="font-size:1.6vw">  CONTACT </small>
+    </h4>
+
+    <div class="empty-space xs-25 sm-25"><br><br></div>
+    <div class="social-icons-footer">
+
+<a class="social-icon-footer linkedin" href="https://mail.google.com/mail/?view=cm&fs=1&to=kunal.jethuri@gmail.com" target="_blank" rel="author">
+<i class="fab fa-google-plus-g"></i>
+    </a>
+
+<br>
+<p style="color:white;><a href=" "></a></p>
+</div>
+    </div>
+  </div>
+  <!-- Social media icons for footer -->
+
+
+
+
+
+
+
+
+  </div>
+    </div>
+  </div>
+</div>
+<div class="tt-copy">
+  <div class="container col-sm-6">
+   <!-- <center class="text-animation">Made with <i class="fa fa-heart" style="color: red;"></i></center>-->
+  </div>
+</div>
+</footer>`);
+let bodyElement = $(`body`);
+bodyElement.prepend(header);
+bodyElement.append(footer);
+
+/*JavaScript for toggle for light/dark mode*/
+
+var checkbox = document.querySelector('input[name=theme]');
+if(checkbox)
+ {
+ checkbox.addEventListener('change', function(){
+   if(this.checked)
+   {
+     trans()
+     document.documentElement.setAttribute('data-theme', 'dark')
+   }
+   else
+   {
+     trans()
+     document.documentElement.setAttribute('data-theme', 'light')
+   }
+ });
+
+ let trans = () => {
+   document.documentElement.classList.add('transition');
+   window.setTimeout(() => {
+     document.documentElement.classList.remove('transition')
+   }, 1000)
+ }}
